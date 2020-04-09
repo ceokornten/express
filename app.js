4a2a44731f243f2f68981ae50c827d4746b9fec5
Chatchawan Chaona <ceokornten@Chatchawans-MacBook-Pro.local>
2020-04-10 00:01:49 +0700

    add new file

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(30001 , function(req,res){
+    console.log("This server run on port 30001");
+});
